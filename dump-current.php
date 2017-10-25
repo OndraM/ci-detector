99c66d5f2445e3ccec68e7f6b2ad0a629f1d5eec
Ondřej Machulda <ondrej.machulda@gmail.com>
2017-10-25 10:50:06 +0200

    Simplify dump script

--- dump-current.php
+++ dump-current.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 // Dump values from current environment
 
@@ -12,15 +12,17 @@ if (!$ciDetector->isCiDetected()) {
 
 $ci = $ciDetector->detect();
 
-echo 'Output of "$ci->getCiName()":' . "\n";
-var_dump($ci->getCiName());
-echo 'Output of "$ci->getBuildNumber()":' . "\n";
-var_dump($ci->getBuildNumber());
-echo 'Output of "$ci->getBuildUrl()":' . "\n";
-var_dump($ci->getBuildUrl());
-echo 'Output of "$ci->getGitCommit()":' . "\n";
-var_dump($ci->getGitCommit());
-echo 'Output of "$ci->getGitBranch()":' . "\n";
-var_dump($ci->getGitBranch());
-echo 'Output of "$ci->getRepositoryUrl()":' . "\n";
-var_dump($ci->getRepositoryUrl());
+$methodsToDump = [
+    'getCiName',
+    'getBuildNumber',
+    'getBuildUrl',
+    'getGitCommit',
+    'getGitBranch',
+    'getRepositoryUrl',
+];
+
+foreach ($methodsToDump as $method) {
+    echo '$ci->' . $method . '()' . "\n";
+    var_dump($ci->$method());
+    echo "\n";
+}
