24375e3317c076951be85b3f342d4d53105da787
Ondřej Machulda <ondrej.machulda@gmail.com>
2021-01-22 19:32:52 +0100

    Don't try to be smart when dumping values

--- dump-current.php
+++ dump-current.php
@@ -7,27 +7,27 @@ require_once __DIR__ . '/vendor/autoload.php';
 $ciDetector = new \OndraM\CiDetector\CiDetector();
 
 if (!$ciDetector->isCiDetected()) {
-    exit('No CI detected');
+    exit('No CI detected' . "\n");
 }
 
 $ci = $ciDetector->detect();
 
-$methodsToDump = [
-    'getCiName',
-    'getBuildNumber',
-    'getBuildUrl',
-    'getGitCommit',
-    'getGitBranch',
-    'getRepositoryName',
-    'getRepositoryUrl',
-];
+echo '$ci->getCiName()' . "\n";
+var_dump($ci->getCiName());
+echo "\n" . '$ci->getBuildNumber()' . "\n";
+var_dump($ci->getBuildNumber());
+echo "\n" . '$ci->getBuildUrl()' . "\n";
+var_dump($ci->getBuildUrl());
+echo "\n" . '$ci->getCommit()' . "\n";
+var_dump($ci->getCommit());
+echo "\n" . '$ci->getBranch()' . "\n";
+var_dump($ci->getBranch());
+echo "\n" . '$ci->getTargetBranch()' . "\n";
+var_dump($ci->getTargetBranch());
+echo "\n" . '$ci->getRepositoryName()' . "\n";
+var_dump($ci->getRepositoryName());
+echo "\n" . '$ci->getRepositoryUrl()' . "\n";
+var_dump($ci->getRepositoryUrl());
 
-foreach ($methodsToDump as $method) {
-    echo '$ci->' . $method . '()' . "\n";
-    var_dump($ci->$method());
-    echo "\n";
-}
-
-echo '$ci->isPullRequest()->describe()' . "\n";
+echo "\n" . '$ci->isPullRequest()->describe()' . "\n";
 var_dump($ci->isPullRequest()->describe());
-echo "\n";
